bdbb1e784b8934a397edd585c892db0f56a903dc
Stefanny <stefanny@email.com>
2026-07-19 19:44:55 -0300

    fix: ruta raiz

--- server.js
+++ server.js
@@ -95,6 +95,10 @@ app.post("/api/chat", async (req, res) => {
   }
 });
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.get("/{*path}", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
